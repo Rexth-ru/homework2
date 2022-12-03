e3f9be73ae1eba40d47740ba44335854e58267d9
Rexth-ru <annakucher8240@gmail.com>
2022-12-03 20:18:39 +0300

    Переделано дз

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        int a1 = 5;
-        int b1 = 7;
+        int a1 = 10;
+        int b1 = 13;
         int a2 = 5;
-        int b2 = 12;
-        if (a1 < a2 ) {
-            if (a2 < b1 && b1 <= b2) {
+        int b2 = 10;
+        if (a1 < a2 && b1 < b2) {
+            if (a2 < b1) {
                 System.out.println("Координаты отрезка пересечения [" + a2 + ";" + b1 + "]");
             }
-            if (a2 == b1 && b1 < b2) {
+            if (a2 == b1) {
                 System.out.println("Точка пересечения отрезков " + a2);
             }
-            if (a2 < b1 && b2 < b1) {
-                System.out.println("Координаты отрезка пересечения [" + a2 + ";" + b2 + "]");
-            } else {
-            System.out.println("Отрезки не пересекаются");
+            if (b1 < a2) {
+                System.out.println("Отрезки не пересекаются");
             }
         } else {
-            if (a1 < b2 && b2 <= b1){
-                System.out.println("Координаты отрезка пересечения ["+ a1 +";"+ b2 +"]");
+            if (a1 < a2 && b2 < b1) {
+                System.out.println("Координаты отрезка пересечения [" + a2 + ";" + b2 + "]");
             }
-            if (a1 == b2 && b2<b1){
-                System.out.println("Точка пересечения отрезков "+ a1);
+        }
+        if (a2 < a1 && b2 < b1) {
+            if (a1 < b2) {
+                System.out.println("Координаты отрезка пересечения [" + a1 + ";" + b2 + "]");
             }
-            if (a1 < b2 && b1 < b2) {
-                System.out.println("Координаты отрезка пересечения ["+a1+";"+b1+"]");
-            }else {
+            if (a1 == b2) {
+                System.out.println("Точка пересечения отрезков " + a1);
+            }
+            if (b2 < a1) {
                 System.out.println("Отрезки не пересекаются");
+            }
+        } else {
+            if (a2 < a1 && b1 < b2) {
+                System.out.println("Координаты отрезка пересечения [" + a1 + ";" + b1 + "]");
             }
         }
     }
