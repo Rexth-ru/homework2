ca1f6b834329063bd353e1b47bb2f285ff52784f
Rexth-ru <annakucher8240@gmail.com>
2022-12-04 12:49:56 +0300

    Доработано дз

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int a1 = 10;
-        int b1 = 13;
-        int a2 = 5;
-        int b2 = 10;
-        if (a1 < a2 && b1 < b2) {
+        int a1 = 1;
+        int b1 = 4;
+        int a2 = 0;
+        int b2 = 5;
+        if (a1 <=a2 && b1 <=b2) {
             if (a2 < b1) {
                 System.out.println("Координаты отрезка пересечения [" + a2 + ";" + b1 + "]");
             }
@@ -19,7 +19,7 @@ public class Main {
                 System.out.println("Координаты отрезка пересечения [" + a2 + ";" + b2 + "]");
             }
         }
-        if (a2 < a1 && b2 < b1) {
+        if (a2 <= a1 && b2 <= b1) {
             if (a1 < b2) {
                 System.out.println("Координаты отрезка пересечения [" + a1 + ";" + b2 + "]");
             }
